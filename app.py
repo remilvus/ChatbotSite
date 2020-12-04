85616610c4a5f713576fd785de9f77134fa7d93d
Adam Kania <remilvus@gmail.com>
2020-12-04 22:45:48 +0100

    add webhook recognition and responses

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, jsonify
-from spreadsheet_management import add_rhyme, add_complaint
+from flask import Flask, render_template, jsonify, request
+from spreadsheet_management import add_rhyme, add_complaint, save_rhyme_to_file, load_rhyme_from_file
 import threading
 
 app = Flask(__name__)
-
+save_rhyme_to_file(wait=True)
 
 @app.route('/')
 def index():
@@ -11,9 +11,35 @@ def index():
 
 @app.route('/webhook', methods=['POST'])
 def respond():
-    add_rhyme("AAAAAAAAAAAAAA")
-    data = {"fulfillmentMessages": [{"text": { "text": [ "Text response from THE webhook"]}}]}
-    return jsonify(data), 200
+    data = request.get_json(silent=True)
+    request_type = data['queryResult']['intent']['displayName']
+    
+    if request_type == 'Complaint':
+        request_content = data['queryResult']['parameters']['any']
+        add_complaint(request_content)
+        reply = {
+            "fulfillmentText": "Żałujemy, że Cię to spotkało. Postaramy się poprawić.",
+        }
+        return jsonify(reply), 200
+
+    elif request_type == 'Rhyme':
+        request_content = data['queryResult']['parameters']['any']
+        add_rhyme(request_content)
+        reply = {
+            "fulfillmentText": "Piękna przyśpiewka, zostanie dodana do naszej kolekcji :D",
+        }
+        return jsonify(reply), 200
+        
+    elif request_type == 'GetRhyme':
+        rhyme = load_rhyme_from_file()
+        save_rhyme_to_file()
+        reply = {
+            "fulfillmentText": rhyme,
+        }
+        return jsonify(reply), 200
+
+    reply = {"fulfillmentText": "Coś poszło nie tak - nie rozumiem o co chodzi",}
+    return jsonify(reply), 200
 
 
 if __name__ == '__main__': 
--- spreadsheet_management.py
+++ spreadsheet_management.py
@@ -1,10 +1,10 @@
 import auth
 import threading
+from random import randint
 
 SPREADSHEET_ID = '1BkeZPUWZ87pGjaAkgoRSOeBhuOSn0Nw9V856wOgWZsM'
 RHYME_NAME = "rhymes"
 COMPLAINT_NAME = "complaints"
-RANGE = 'B1:B100'
 INDEX_COL = "A"
 INDEX_ROW = "1"
 DATA_COLUMN = "B"
@@ -48,6 +48,34 @@ def add_rhyme(rhyme):
     t.deamon = True
     t.start()
 
+def _save_rhyme_to_file():
+    sheet = auth.get_service().spreadsheets()
+    free_idx = get_free_index(sheet, RHYME_NAME)
+
+    rhyme = ""
+    if free_idx <= 1:
+        rhyme = "Niestety, ale nic nie mam :/" 
+    else:
+        idx = randint(1, free_idx-1)
+        rhyme = get_cell(sheet, RHYME_NAME + "!" + DATA_COLUMN + str(idx))
+
+    with open("rhyme.txt", 'w') as f:
+        f.write(rhyme)
+
+def save_rhyme_to_file(wait=False):
+    t = threading.Thread(target=_save_rhyme_to_file, args=[])
+    t.deamon = True
+    t.start()
+    if wait:
+        t.join()
+
+def load_rhyme_from_file():
+    rhyme = ""
+    with open("rhyme.txt", 'r') as f:
+        rhyme = f.read()
+
+    return rhyme
+
 def add_complaint(complaint):
     t = threading.Thread(target=add_text, args=[complaint, COMPLAINT_NAME])
     t.deamon = True
@@ -56,14 +84,3 @@ def add_complaint(complaint):
 
 if __name__=="__main__":
     pass
-
-    # result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
-    #                             range=RANGE).execute()
-    # values = result.get('values')
-    # up = sheet.values().update(spreadsheetId=SPREADSHEET_ID, range="A1:B2", body=update_body, valueInputOption="USER_ENTERED")
-    # print(up.execute())
-    # if not values:
-    #     print('No data found.')
-    # else:
-    #     for row in values:
-    #         print(row)
