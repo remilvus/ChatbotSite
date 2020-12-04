f979bfb63e92f598a46855ccb06498449211078b
Adam Kania <remilvus@gmail.com>
2020-12-04 14:05:15 +0100

    add test link

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+    
+@app.route('/test')
+def index():
+    return render_template('index.html')
 
 
 @app.route('/webhook', methods=['POST'])
