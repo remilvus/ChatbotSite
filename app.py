1a9fb7435131bef51a302445fb6cea16e154eee5
Adam Kania <remilvus@gmail.com>
2020-12-04 19:46:07 +0100

    add google sheets editing

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ def test():
 
 @app.route('/webhook', methods=['POST'])
 def respond():
-    #add_rhyme('>rhyme')
-    #add_complaint('>complaint')
+    add_rhyme('>rhyme')
+    add_complaint('>complaint')
 
     data = {"fulfillmentMessages": [
 	    {
