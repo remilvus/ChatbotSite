8be978cfde85d794610164a5a02196bdb69299ee
Adam Kania <remilvus@gmail.com>
2020-12-04 20:20:48 +0100

    add google sheets editing

--- auth.py
+++ auth.py
@@ -22,7 +22,7 @@ def get_service():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                'google-credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
